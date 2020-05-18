e6d0433b074f40f4f26604f6b6e650f69b029139
Szabolcs Surányi <szabolcs.suranyi@shadesoft.hu>
2020-05-18 23:16:22 +0200

    Debug mode on

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from flask import request
 from pdf2image import convert_from_path
 
 app = flask.Flask(__name__)
-#app.config['DEBUG'] = True
+app.config['DEBUG'] = True
 
 @app.route('/', methods=['GET'])
 def home():
